90bc420506e31a0ff6b14e93ab2dcf2c641d6c92
hemaang <aadisahayjamuar>
2025-12-08 10:33:31 +0530

    corrected main

---
+++ loginpage.js
@@ -0,0 +1,13 @@
+
+        function login(){
+            let user = document.getElementById("user-name").value;
+            let pass = document.getElementById("user-password").value;
+
+            if(user === "2500032036@kluniversity" && pass === "hemaang sahay 008"){
+                window.location.href = "index.html";  // Redirect to tasks page
+            } 
+            else{
+                alert("Invalid username or password!");
+            }
+        }
+    
